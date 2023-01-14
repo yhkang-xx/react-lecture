79828691a9856868d2d38073ed974810bcc36b89
misso <misso.kang@gmail.com>
2023-01-14 15:51:07 +0900

    react Hook - useState 사용법

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,28 +1,23 @@
-
+import { useState } from "react";
 
 export default function Hello() {
-    function showName() {
-        console.log('Mike');
-    }
-    function showAge(age) {
-        console.log(age);
-    }
-    function showText(txt) {
-        console.log(txt);
-    }
+    const [name, setName] = useState('Mike');
+
+    // function changeName() {
+    //     const newName = name === 'Mike' ? 'Jane' : 'Mike';
+    //     setName(newName);
+    // };
+
     return (
         <div>
-            <h1>Hello</h1>
-            <button onClick={showName}>Show Name</button>
+            {/* <h1>state</h1>
+            <h2>컴포넌트 속성값</h2> */}
+            <h3 id='name'>{name}</h3>
+            {/* <button onClick={changeName}>Change</button> */}
             <button onClick={() => {
-                showAge(30);
-            }}>Show Age</button>
-
-            <input type="text" onChange={e => {
-                const txt = e.target.value;
-                showText(txt);
-            }} />
-
+                setName(name === 'Mike' ? 'Jane' : 'Mike'
+                )
+            }}>Change</button>
         </div>
     );
 };
