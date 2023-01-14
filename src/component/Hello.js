c4ea18ffe685295fdcf89e5e1a8d6064d4e5a24d
misso <misso.kang@gmail.com>
2023-01-14 16:19:55 +0900

    react-props 예제2

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello(props) {
-    console.log(props)
+export default function Hello({ age }) {
     const [name, setName] = useState('Mike');
-    const [age, setAge] = useState(props.age);
+    const msg = age > 19 ? '성인 입니다.' : '미성년자 입니다.'
 
     return (
         <div>
-            <h3 id='name'>{name} ({age})</h3>
+            <h3 id='name'>{name}({age}): {msg}</h3>
+            <UserName name={name} />
             <button
                 onClick={() => {
                     setName(name === 'Mike' ? 'Jane' : 'Mike');
-                    setAge(age + 1);
                 }}
             >Change</button>
         </div>
---
+++ src/component/UserName.js
@@ -0,0 +1,6 @@
+export default function UserNam({ name }) {
+
+    return <p>
+        Hello, {name}
+    </p>
+}
